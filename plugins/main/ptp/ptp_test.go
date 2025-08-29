criteo-forks/plugins

Add features: host netns + route source interface (#5)
* add more features to ptp plugin

* add option "host_netns" in configuration to move the host veth
  interface in a specific netns
* add options "route_source_interface_ipv4/ipv6" to change source IP
  used in routes configured by the IPAM plugin
* add option "sysctl", taking as input the same format of configuration
  than the "tuning" plugin to configure sysctl parameters in the
  container netns

* fix: close destNetns fd

* fix: ensure to convert configuration passed to tuning plugin

* result needs to be converted to the right CNI version before passing
  it to another plugin

* fix: remove link-local ipv6 address from the address list

* when using route_source_interface, we expect to have a single IP
  configured on the interface, however in IPv6, there is always a link
  local address configured in addition to the one provided by the IPAM
  plugin
* getIntfIP function was returning an error as we were seeing 2
  addresses instead of 1, now we filter this link-local address to fix
  it

* tests: add tests for new options on ptp plugin

* fix: handle default route IPv4/IPv6 when replacing src IP

* default route in IPv4 and IPv6 is equivalent to a Dst = nil in a route
  filter

* cover new features introduced in ptp in cmdCheck

* we must check that all the new config parameters have been correctly
  set on the host/container netns

* tests: add routes to the test case config

* fix: defer netns closure after checking error

* tests, lint: fix ineffassign errors raised by linter

* lint: remove extra line

* remove mentions to Criteo

* feat: remove sysctl

* the sysctl feature might be unnecessary as we could configure sysctl
  parameters on the host itself, or via the mesos isolator
* we'll repropose that feature again if this is needed

plugins/main/ptp/ptp_test.go
@@ -17,7 +17,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"net"
 	"os"
+	"path"
 	"strings"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -535,6 +537,103 @@ var _ = Describe("ptp Operations", func() {
 				return nil
 			})
 			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It(fmt.Sprintf("[%s] configures and deconfigures a CNI dual-stack ptp link + custom options with ADD/DEL", ver), func() {
+			const SRCIFNAME = "dummy0"
+			const DUMMYIPV4 = "10.2.3.1/24"
+			const DUMMYIPV6 = "2001:db8:2::1/64"
+
+			hostNS, err := testutils.NewNS()
+			Expect(err).NotTo(HaveOccurred())
+			defer func() {
+				Expect(hostNS.Close()).To(Succeed())
+				Expect(testutils.UnmountNS(hostNS)).To(Succeed())
+			}()
+
+			_, hostNsName := path.Split(hostNS.Path())
+
+			conf := fmt.Sprintf(`{
+				"cniVersion": "%s",
+				"name": "mynet",
+				"type": "ptp",
+				"ipMasq": false,
+				"mtu": 5000,
+				"host_netns": "%s",
+				"route_source_interface_ipv4": "%[3]s",
+				"route_source_interface_ipv6": "%[3]s",
+				"ipam": {
+					"type": "host-local",
+					"ranges": [
+						[{ "subnet": "10.1.2.0/24"}],
+						[{ "subnet": "2001:db8:1::0/66"}]
+					],
+					"routes": [
+						{
+							"dst": "0.0.0.0/0"
+						},
+						{
+							"dst": "::/0"
+						},
+						{
+							"dst": "10.0.0.0/8"
+						},
+						{
+							"dst": "172.16.0.0/12"
+						}
+					]
+				}
+			}`, ver, hostNsName, SRCIFNAME)
+
+			// Add a dummy interface in container netns with IPv4/IPv6
+			// to test the route_source_interface option
+			err = targetNS.Do(func(ns.NetNS) error {
+				defer GinkgoRecover()
+				err := netlink.LinkAdd(&netlink.Dummy{
+					LinkAttrs: netlink.LinkAttrs{
+						Name: SRCIFNAME,
+					},
+				})
+				Expect(err).NotTo(HaveOccurred())
+
+				link, err := netlink.LinkByName(SRCIFNAME)
+				Expect(err).NotTo(HaveOccurred())
+
+				err = netlink.LinkSetUp(link)
+				Expect(err).NotTo(HaveOccurred())
+
+				type dummyAddresses struct {
+					ip  net.IP
+					net *net.IPNet
+				}
+
+				ipv4, netIPv4, err := net.ParseCIDR(DUMMYIPV4)
+				Expect(err).NotTo(HaveOccurred())
+				ipv6, netIPv6, err := net.ParseCIDR(DUMMYIPV6)
+				Expect(err).NotTo(HaveOccurred())
+
+				for _, address := range []dummyAddresses{
+					{
+						ip:  ipv4,
+						net: netIPv4,
+					},
+					{
+						ip:  ipv6,
+						net: netIPv6,
+					},
+				} {
+					err := netlink.AddrAdd(link, &netlink.Addr{
+						IPNet: &net.IPNet{
+							IP:   address.ip,
+							Mask: address.net.Mask,
+						},
+					})
+					Expect(err).NotTo(HaveOccurred())
+				}
+				return nil
+			})
+			Expect(err).NotTo(HaveOccurred())
+			doTest(conf, ver, 2, types.DNS{}, targetNS)
 		})
 	}
 })

criteo-forks/cni-plugins

plugins/main/ptp/ptp_test.go
@@ -17,11 +17,14 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"net"
 	"os"
+	"path"
 	"strings"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/vishvananda/netlink"
 
 	"github.com/containernetworking/cni/pkg/skel"
 	"github.com/containernetworking/cni/pkg/types"
@@ -600,6 +603,103 @@ var _ = Describe("ptp Operations", func() {
 				return nil
 			})
 			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It(fmt.Sprintf("[%s] configures and deconfigures a CNI dual-stack ptp link + custom options with ADD/DEL", ver), func() {
+			const SRCIFNAME = "dummy0"
+			const DUMMYIPV4 = "10.2.3.1/24"
+			const DUMMYIPV6 = "2001:db8:2::1/64"
+
+			hostNS, err := testutils.NewNS()
+			Expect(err).NotTo(HaveOccurred())
+			defer func() {
+				Expect(hostNS.Close()).To(Succeed())
+				Expect(testutils.UnmountNS(hostNS)).To(Succeed())
+			}()
+
+			_, hostNsName := path.Split(hostNS.Path())
+
+			conf := fmt.Sprintf(`{
+				"cniVersion": "%s",
+				"name": "mynet",
+				"type": "ptp",
+				"ipMasq": false,
+				"mtu": 5000,
+				"host_netns": "%s",
+				"route_source_interface_ipv4": "%[3]s",
+				"route_source_interface_ipv6": "%[3]s",
+				"ipam": {
+					"type": "host-local",
+					"ranges": [
+						[{ "subnet": "10.1.2.0/24"}],
+						[{ "subnet": "2001:db8:1::0/66"}]
+					],
+					"routes": [
+						{
+							"dst": "0.0.0.0/0"
+						},
+						{
+							"dst": "::/0"
+						},
+						{
+							"dst": "10.0.0.0/8"
+						},
+						{
+							"dst": "172.16.0.0/12"
+						}
+					]
+				}
+			}`, ver, hostNsName, SRCIFNAME)
+
+			// Add a dummy interface in container netns with IPv4/IPv6
+			// to test the route_source_interface option
+			err = targetNS.Do(func(ns.NetNS) error {
+				defer GinkgoRecover()
+				err := netlink.LinkAdd(&netlink.Dummy{
+					LinkAttrs: netlink.LinkAttrs{
+						Name: SRCIFNAME,
+					},
+				})
+				Expect(err).NotTo(HaveOccurred())
+
+				link, err := netlinksafe.LinkByName(SRCIFNAME)
+				Expect(err).NotTo(HaveOccurred())
+
+				err = netlink.LinkSetUp(link)
+				Expect(err).NotTo(HaveOccurred())
+
+				type dummyAddresses struct {
+					ip  net.IP
+					net *net.IPNet
+				}
+
+				ipv4, netIPv4, err := net.ParseCIDR(DUMMYIPV4)
+				Expect(err).NotTo(HaveOccurred())
+				ipv6, netIPv6, err := net.ParseCIDR(DUMMYIPV6)
+				Expect(err).NotTo(HaveOccurred())
+
+				for _, address := range []dummyAddresses{
+					{
+						ip:  ipv4,
+						net: netIPv4,
+					},
+					{
+						ip:  ipv6,
+						net: netIPv6,
+					},
+				} {
+					err := netlink.AddrAdd(link, &netlink.Addr{
+						IPNet: &net.IPNet{
+							IP:   address.ip,
+							Mask: address.net.Mask,
+						},
+					})
+					Expect(err).NotTo(HaveOccurred())
+				}
+				return nil
+			})
+			Expect(err).NotTo(HaveOccurred())
+			doTest(conf, ver, 2, types.DNS{}, targetNS)
 		})
 	}
 })